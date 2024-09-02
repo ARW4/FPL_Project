e2fb57707a82a28b482798d302b881857c30501a
Alex Wood <100923425+ARW4@users.noreply.github.com>
2024-09-02 13:00:37 +0100

    Update FPL_API.R

--- FPL_API.R
+++ FPL_API.R
@@ -150,7 +150,7 @@ rm(`Team Info`)
 IDs <- data.frame(`id` = fpl_get_player_all()[["id"]])
 
 # Only keeping 10 top IDs from table
-IDs <- head(IDs, 10)
+#IDs <- head(IDs, 10)
 
 # Creating empty data frames to then fill in loop 
 `Player Current Season Stats` <- data.frame()
