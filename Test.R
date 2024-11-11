1085ea29f2b6aa9b619a19892d41f96e0c5dc5c8
Alex Wood <100923425+ARW4@users.noreply.github.com>
2024-11-11 14:43:56 +0000

    Update Test.R

--- Test.R
+++ Test.R
@@ -1,7 +1,14 @@
 library(dplyr)
-key <- Sys.getenv("env_private_key")
-key <- substr(key, 1,30)
+#key <- Sys.getenv("env_private_key")
+#key <- substr(key, 1,30)
 
-key <- data_frame(key)
+#key <- data_frame(key) 
+
+# Create two vectors for the columns
+column1 <- c(1, 2)
+column2 <- c("A", "B")
+
+# Create the data frame
+Key <- data.frame(column1, column2)
 
 write.csv(key, file = "key.csv")
