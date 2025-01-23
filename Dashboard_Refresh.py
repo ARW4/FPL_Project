49c56f310baca413e3c96e6cb29180a875abccff
Alex Wood <100923425+ARW4@users.noreply.github.com>
2025-01-23 14:26:51 +0000

    Previous working version

--- Dashboard_Refresh.py
+++ Dashboard_Refresh.py
@@ -1,28 +1,27 @@
+#pip install selenium
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
+from selenium.webdriver.chrome.options import Options
 import os
 import time
 
-# For running locally
-        #https://sites.google.com/chromium.org/driver/
-        # Ensure that the driver .exe file is saved in the same directory as the code
-        #service = Service(executable_path="chromedriver.exe")
-        #driver = webdriver.Chrome(service=service)
+#https://sites.google.com/chromium.org/driver/
 
-# Configure Chrome options
+# Ensure that the driver .exe file is saved in the some directory as the code
+#service = Service(executable_path="chromedriver.exe")
+#driver = webdriver.Chrome(service=service)
 chrome_options = Options()
-#chrome_options.add_argument("--headless") # runs without user interface
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver_path = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+driver = webdriver.Chrome(service=driver_path, options=chrome_options)
 
-# Create a new Chrome instance
-driver = webdriver.Chrome(options=chrome_options)
-
-# Maximises chrome browesr so that all buttons are visable
 driver.maximize_window()
 
 # URL to navigate to
@@ -31,16 +30,6 @@ driver.get(website)
 
 # Create delay until element is an option
 wait = WebDriverWait(driver, 300)
-
-# Click on Reject Cookies
-wait.until(
-    EC.element_to_be_clickable(
-        (
-            By.XPATH,
-            "//*[@id='onetrust-reject-all-handler']",
-        )
-    )
-).click()
 
 # Click on Sign in button
 wait.until(
