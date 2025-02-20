8cde8e0b860c6259bfecd7c0bac3451d1e366f4f
Alex Wood <100923425+ARW4@users.noreply.github.com>
2025-02-20 09:23:23 +0000

    Update Dashboard_Refresh.py

--- Dashboard_Refresh.py
+++ Dashboard_Refresh.py
@@ -29,7 +29,7 @@ website = "https://public.tableau.com/app/profile/alexrwood/viz/FPLDashboard_172
 driver.get(website)
 
 # Create delay until element is an option
-wait = WebDriverWait(driver, 300)
+wait = WebDriverWait(driver, 10)
 
 # Click on Sign in button
 wait.until(
