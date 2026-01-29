a541de1dd33480a35aa20934d614c0d5d424acea
Alex Wood <100923425+ARW4@users.noreply.github.com>
2026-01-29 10:31:55 +0000

    Update dashboard_refresh_script.py

--- dashboard_refresh_script.py
+++ dashboard_refresh_script.py
@@ -13,7 +13,7 @@ import shutil
 
 # Set Driver
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+#chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
